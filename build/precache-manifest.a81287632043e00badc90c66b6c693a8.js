789a35c20b64da80243e3342297b2c9e8b0a9640
Pranay <matepranay11@gmail.com>
2020-06-29 16:33:45 +0530

    first commit

---
+++ build/precache-manifest.a81287632043e00badc90c66b6c693a8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52109dede5cc420bb0c7464a0810712a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e18802fcdcefcb9fb6",
+    "url": "/static/css/2.fa2b5f87.chunk.css"
+  },
+  {
+    "revision": "8d18ccb273a490603d5e",
+    "url": "/static/css/main.c9cf53c6.chunk.css"
+  },
+  {
+    "revision": "16e18802fcdcefcb9fb6",
+    "url": "/static/js/2.f677cc2a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f677cc2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d18ccb273a490603d5e",
+    "url": "/static/js/main.d797a487.chunk.js"
+  },
+  {
+    "revision": "fa5f104a25901f0f58ad",
+    "url": "/static/js/runtime-main.b97f594c.js"
+  },
+  {
+    "revision": "c7d575e7558ad50f6f40ff379325b142",
+    "url": "/static/media/clouds+mountains.c7d575e7.jpg"
+  },
+  {
+    "revision": "dea9f4273efb2e705ef1f1f03096e0a2",
+    "url": "/static/media/group.dea9f427.jpg"
+  },
+  {
+    "revision": "92029924301faf208082717c2b5ff046",
+    "url": "/static/media/hills.92029924.jpg"
+  },
+  {
+    "revision": "2db1e16958e500c3329038d270db8997",
+    "url": "/static/media/lohgad.2db1e169.jpg"
+  },
+  {
+    "revision": "89ce69c774459d71a28f75d8dfc7195e",
+    "url": "/static/media/man.89ce69c7.jpg"
+  },
+  {
+    "revision": "89f5d5f768167c979632c27389fd03f4",
+    "url": "/static/media/other.89f5d5f7.jpg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "13cc70cc0eb00f304d7237a5a99352ff",
+    "url": "/static/media/woman.13cc70cc.jpg"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+var request = require("request");
+var cors = require('cors');
+var multer  = require('multer');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/webuDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+/////////////////////////////REVIEW///////////
+const reviewSchema = {
+  picture: {
+      type: String
+  },
+    username: {
+        type: String,
+        required: true
+    },
+    trekname: {
+        type: String,
+        required: true
+    },
+    review: {
+        type: String,
+        required: true
+    }
+};
+
+const Review = mongoose.model('Review', reviewSchema);
+
+
+app.use(cors());
+app.get("/review", function(req, res) {
+  Review.find({}, function(err, foundReview){
+    //console.log(foundReview);
+    res.send(foundReview);
+});
+});
+
+app.post("/addReview", function(req, res) {
+  var review = req.body;
+  console.log(review);
+  Review.insertMany(review, function(err){
+    if(err){
+      console.log(err);
+
+    }else{
+      console.log("successfuly add items+");
+    }
+  });
+  Review.find({}, function(err, foundReview){
+  //  console.log(foundReview);
+    res.send(foundReview);
+  });
+});
+/////////////////////////////TREKENTRY///////////
+const trekSchema = {
+  username: {
+      type: String,
+      required: true
+  },
+  trekname: {
+      type: String,
+      required: true
+  },
+  selectedDay: {
+      type: String,
+      required: true
+  },
+  selectedNight: {
+      type: String,
+      required: true
+  },
+  selectedDate: {
+      type: String,
+      required: true
+  },
+  height_FT: {
+      type: String,
+      required: true
+  },
+  height_MT: {
+      type: String,
+      required: true
+  },
+  selectedDifficulty: {
+      type: String,
+      required: true
+  },
+
+};
+
+const Trek = mongoose.model('Trek', trekSchema);
+app.get("/trek/trekdetails/:trekId", function(req, res) {
+  console.log(req.params.trekId);
+  Trek.findById(req.params.trekId)
+  .then((trek) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      console.log(trek);
+      res.json(trek);
+  })
+
+});
+app.post("/addTrekEntry", function(req, res) {
+  var TrekDetails = req.body;
+  Trek.insertMany(TrekDetails, function(err){
+    if(err){
+      console.log(err);
+
+    }else{
+      console.log("successfuly add items+");
+    }
+  });
+  Trek.find({}, function(err, foundTrekDetails){
+    console.log(foundTrekDetails);
+    res.send(foundTrekDetails);
+  });
+
+});
+
+app.use(express.static(path.join(__dirname, './build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build'))
+})
+app.listen(port,console.log("hosted on port:"+port));
