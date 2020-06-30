c2b3a07cfa493bcf28b0dd61f26361ce22b3de1e
Pranay <matepranay11@gmail.com>
2020-06-30 22:06:39 +0530

    1

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Pranay:FN4hg5EPm4iR8ifQ@cluster0.h2oju.mongodb.net/webuDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/webuDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 /////////////////////////////REVIEW///////////
 const reviewSchema = {
